0a2e1db4ae1f4fe80f814f0c31d53c257ae22c16
Jeferson Santos <jefss@outlook.com>
2026-02-25 11:29:43 -0300

    :tada: feat: meu primeiro servidor

---
+++ primeiraaula/server.js
@@ -0,0 +1,20 @@
+// Importamos express
+const express = require('express');
+
+// criando uma aplicação express, aqui estamos inicializando o nosso servidor
+const app = express()
+const PORT = 3000
+
+//"/" significa rota principal (http://localhost:3000/)
+app.get("/", (req, res) => {
+    // req = request (dados enviados pelo cliente)
+    // res = response (resposta que vamos enviar)
+    
+    res.send('Olá, mundo! Bem vindo a aula de backend!')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}/`)
+})
