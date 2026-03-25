d105f4adfe97978735790e8f0a30188a8c7a92c9
Jeferson Santos <jefss@outlook.com>
2026-03-25 10:37:20 -0300

    :rocket: feat: Revisão e criação de rota POST

--- api-orkut/src/app.js
+++ api-orkut/src/app.js
@@ -8,11 +8,24 @@ app.get("/", (req, res) => {
     res.send("<h1>Rede Social!</h1>")
 });
 
+app.get("/usuarios", async (req, res) => {
+    try {
+        const resultado = await pool.query(`
+            SELECT * FROM usuarios;
+        `);
+        res.json(resultado.rows)
+    } catch (erro) {
+        res.status(500).json({erro: "Erro ao buscar dados de usuários"});
+    }
+});
+
 app.get("/posts", async (req, res) => {
     try {
         const resultado = await pool.query(`
             SELECT
+                usuarios.id,
                 usuarios.nome,
+                post.titulo,
                 post.conteudo,
                 post.criado_em
             FROM post
@@ -26,5 +39,28 @@ app.get("/posts", async (req, res) => {
         res.status(500).json({erro: "Erro ao buscar postagens"})
     }
 })
+
+app.post("/post", async (req, res) => {
+  try {
+    const { titulo, conteudo, usuario_id } = req.body;
+    const resultado = await pool.query(
+      `
+      INSERT INTO post (titulo, conteudo, usuario_id)
+      VALUES ($1, $2, $3)
+      RETURNING *
+      `,
+      [titulo, conteudo, usuario_id],
+    );
+    res.status(201).json({
+      mensagem: "Post criado com sucesso",
+      post: resultado.rows[0],
+    });
+  } catch (erro) {
+    res.status(500).json({
+      erro: "Erro ao criar postagem",
+    });
+  }
+});
+
 
 module.exports = app;
